537728a6bf0ffc3028e71166eb4edf872d6d67ee
Lucjan Izworski <lucjan.izworski@gotoma.pl>
2022-12-05 15:12:49 +0100

    End of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        public DataContext _context { get; }
+        public BuggyController(DataContext context){
+            _context = context;
+        }
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret(){
+            return "secret text";
+        }
+        [Authorize]
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound(){
+            var thing = _context.Users.Find(-1);
+            if(thing == null)  return NotFound();
+            return thing ;
+        }
+        [Authorize]
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError(){
+                var thing = _context.Users.Find(-1);
+                var thingToReturn = thing.ToString();
+                return thingToReturn;
+        }
+        [Authorize]
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest(){
+            return BadRequest("This was not a good request");
+        }
+
+
+    }
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -13,6 +13,10 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +27,10 @@ import { SharedModule } from './_modules/shared.module';
     MemberListComponent,
     MemberDetailComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +41,9 @@ import { SharedModule } from './_modules/shared.module';
     SharedModule
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
